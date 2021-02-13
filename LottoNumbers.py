75829a1d42bce090e61af9630ff791436255c4f6
saikiran90 <44542994+saikiran90@users.noreply.github.com>
2021-02-13 14:06:49 -0600

    Added a dot

--- LottoNumbers.py
+++ LottoNumbers.py
@@ -7,7 +7,7 @@ DAYS_IN_WEEK = 7
 
 def find(number_of_days):
     # Assume that years is
-    # of 365 days
+    # of 365 days.
     year = int(number_of_days / 365)
     week = int((number_of_days % 365) /
                DAYS_IN_WEEK)
